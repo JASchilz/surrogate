f75f0e90e64bc931c0d57d63a224e7e4b38ee296
Joseph Schilz <joseph@schilz.org>
2019-04-10 21:09:16 -0700

    Restructure for publication.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='surrogate',
+      version='0.1',
+      description='A Python micro-lib to create stubs for non-existing modules.',
+      url='http://github.com/JASchilz/surrogate',
+      author='Kostia Balitsky',
+      packages=['surrogate'],
+      zip_safe=False)
